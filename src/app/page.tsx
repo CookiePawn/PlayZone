ad0506ec82c91a81048b84e341dd7be9b7aa17c8
CookiePawn <cookiepawn@naver.com>
2025-04-09 23:16:17 +0900

    [add][Gemini][Gemini 연동 테스트]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ const RootPage = () => {
       router.push('/home'); // Redirect to /home after loading
     }, 1000); // 1 second delay
 
-    console.log('NEXT_PUBLIC_API_KEY', process.env.NEXT_PUBLIC_API_KEY);
+    console.log('NEXT_PUBLIC_GEMINI_API_KEY', process.env.NEXT_PUBLIC_GEMINI_API_KEY);
 
     // Cleanup the timer if the component unmounts
     return () => clearTimeout(timer);
