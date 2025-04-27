28025c8f805c11f8da21545d00c7af05f8003edf
CookiePawn <cookiepawn@naver.com>
2025-04-27 15:36:15 +0900

    [new][ads][add ads txt]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -16,7 +17,32 @@ export default function RootLayout({
 }) {
     return (
         <html lang="ko">
+            <head>
+                <Script id="google-tag-manager" strategy="afterInteractive">
+                    {`
+                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-KR3WJCRT');
+                    `}
+                </Script>
+                <Script
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7246292657634923"
+                    strategy="afterInteractive"
+                    crossOrigin="anonymous"
+                />
+                <meta name="google-adsense-account" content="ca-pub-7246292657634923" />
+            </head>
             <body className={inter.className}>
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-KR3WJCRT"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
                 {children}
             </body>
         </html>
