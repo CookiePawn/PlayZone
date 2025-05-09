06f91619d9d9dfdc0fa870b967679cfd343aaa20
CookiePawn <cookiepawn@naver.com>
2025-05-09 11:24:39 +0900

    [fix][meta][fix meta setting]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,23 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: 'AI 놀이터',
-    description: 'Your hub for exciting side projects!',
+    title: '플레이존 - AI 컨텐츠 놀이터',
+    description: '플레이존은 AI 컨텐츠 놀이터로, 다양한 AI 컨텐츠를 즐길 수 있는 공간입니다.',
+    keywords: 'AI, 컨텐츠, 놀이터, 플레이존, GEMINI, 챗봇, 챗GPT, 챗봇 놀이터, 퀴즈',
+    openGraph: {
+        title: '플레이존 - AI 컨텐츠 놀이터',
+        description: '플레이존은 AI 컨텐츠 놀이터로, 다양한 AI 컨텐츠를 즐길 수 있는 공간입니다.',
+        url: 'https://ai-playzone.com',
+        siteName: '플레이존 - AI 컨텐츠 놀이터',
+        type: 'website',
+        // images: [
+        //     {
+        //         url: '/images/og-image.png',
+        //         width: 1200,
+        //         height: 630,
+        //     },
+        // ],
+    },
 };
 
 export default function RootLayout({
@@ -18,18 +33,6 @@ export default function RootLayout({
     return (
         <html lang="ko">
             <head>
-                <title>플레이존 - AI 컨텐츠 놀이터</title>
-                <meta name="description" content="플레이존은 AI 컨텐츠 놀이터로, 다양한 AI 컨텐츠를 즐길 수 있는 공간입니다." />
-                <meta name="keywords" content="AI, 컨텐츠, 놀이터, 플레이존, GEMINI, 챗봇, 챗GPT, 챗봇 놀이터, 퀴즈" />
-                <meta property="og:title" content="플레이존 - AI 컨텐츠 놀이터" />
-                <meta property="og:description" content="플레이존은 AI 컨텐츠 놀이터로, 다양한 AI 컨텐츠를 즐길 수 있는 공간입니다." />
-                {/* <meta property="og:image" content="/images/og-image.png" />
-                <meta property='og:image:width' content='1200' />
-                <meta property='og:image:height' content='630' /> */}
-                <meta property="og:url" content="https://ai-playzone.com" />
-                <meta property="og:site_name" content="플레이존 - AI 컨텐츠 놀이터" />
-                <meta property="og:type" content="website" />
-                
                 <Script id="google-tag-manager" strategy="afterInteractive">
                     {`
                         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
